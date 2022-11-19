72d40b1018ea80d979d43e47d0b8d4cfc7b066f2
Water.Li <lizongbin@hotmail.com>
2022-11-19 16:09:43 +0800

    backup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './app.less';
 import { App } from './playground/main';
 import reportWebVitals from './reportWebVitals';
 
